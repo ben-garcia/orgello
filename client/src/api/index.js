d52c79048eadfad5623855d0d73f2b35c70874fe
= <7rubengarcia7@gmail.com>
2019-04-17 14:26:26 -0700

    committed fixed merge conflict in 'src/api/index'

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,10 +1,5 @@
 // server api endpoints
 
-<<<<<<< HEAD
-// authentication
-const authUrl = 'http://localhost:9000/auth/';
-=======
 const authUrl = 'http://localhost:9000/auth';
->>>>>>> client
 
 export default authUrl;
