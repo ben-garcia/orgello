f1cc989867248b5d005f11de23a73d9e9870aba1
= <7rubengarcia7@gmail.com>
2019-04-17 11:43:19 -0700

    added 'eslint-config-prettier' and ' eslint-plugin-prettier' on the server

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -1,7 +1,10 @@
 module.exports = {
-  extends: ['airbnb-base', 'prettier'],
+  extends: ['airbnb-base', 'plugin:prettier/recommended'],
+  env: {
+    node: true,
+  },
   rules: {
     'no-console': 0,
-    'no-restricted-globals': [0],
+    'prettier/prettier': 'error',
   },
 };
