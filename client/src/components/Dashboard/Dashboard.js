eae2f344569b8d2fec62df3a08fca556db2353c4
= <7rubengarcia7@gmail.com>
2019-04-18 16:11:01 -0700

    added basic markup for the Dashboard component

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -1,8 +1,32 @@
 import React from 'react';
 
+import './Dashboard.scss';
+
 const Dashboard = () => (
-  <section>
-    <h1>Welcome to your Dashboard</h1>
+  <section className="dashboard">
+    <section className="dashboard__inner">
+      <ul className="dashboard__nav">
+        <li className="dashboard__link">
+          <i className="fab fa-trello" />
+          <span className="icon-text">Boards</span>
+        </li>
+        <li className="dashboard__link">
+          <i className="far fa-user" />
+          <span className="icon-text">Profile</span>
+        </li>
+        <li className="dashboard__link">
+          <i className="fas fa-cog" />
+          <span className="icon-text">Settings</span>
+        </li>
+      </ul>
+    </section>
+    <section className="boards">
+      <ul className="boards__inner">
+        <li className="boards__item">
+          <span>Create a Board</span>
+        </li>
+      </ul>
+    </section>
   </section>
 );
 
