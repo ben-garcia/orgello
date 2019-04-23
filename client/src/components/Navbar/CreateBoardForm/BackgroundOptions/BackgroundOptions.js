f24d8225edf0e865358c1ec86d75093921bbd254
= <7rubengarcia7@gmail.com>
2019-04-23 14:45:34 -0700

    added styles for photos and colors options

--- client/src/components/Navbar/CreateBoardForm/BackgroundOptions/BackgroundOptions.js
+++ client/src/components/Navbar/CreateBoardForm/BackgroundOptions/BackgroundOptions.js
@@ -1,6 +1,6 @@
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 /* eslint-disable jsx-a11y/no-noninteractive-element-interactions */
-import React from 'react';
+import React, { useState } from 'react';
 import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
@@ -15,68 +15,131 @@ const BackgroundOptions = ({
   changeBoardBackground,
   latestSixPhotos,
 }) => {
+  const [isColorsOptionsOpen, toggleColorsOptions] = useState(false);
+  const [isPhotosOptionsOpen, togglePhotosOptions] = useState(false);
+
   const boardBackgroundKey = Object.keys(currentCreateBoardBackground)[0];
   const boardBackgroundValue = Object.values(currentCreateBoardBackground)[0];
+
+  let title = 'Board Background';
+  let modifiedColors = colors;
+
+  if (isColorsOptionsOpen) {
+    title = 'Colors';
+  } else {
+    // render the first six colors
+    modifiedColors = colors.filter((color, index) => index < 6);
+  }
+
+  if (isPhotosOptionsOpen) {
+    title = 'Photos';
+  }
 
   return (
     <div className="background-options">
       <div className="background-options__header">
-        <span className="background-options__title">Board Background</span>
+        {isPhotosOptionsOpen || isColorsOptionsOpen ? (
+          <button
+            className="background-options__button"
+            type="button"
+            onClick={() => {
+              if (isPhotosOptionsOpen) {
+                togglePhotosOptions(false);
+              }
+              if (isColorsOptionsOpen) {
+                toggleColorsOptions(false);
+              }
+            }}
+          >
+            <i className="fas fa-arrow-left" />
+          </button>
+        ) : null}
+        {/* <span className="background-options__title">{title}</span> */}
+        <span
+          className="background-options__title"
+          style={
+            !isPhotosOptionsOpen && !isColorsOptionsOpen
+              ? { marginLeft: '5rem' }
+              : {}
+          }
+        >
+          {title}
+        </span>
         <button className="background-options__button" type="button">
           <i className="fas fa-times" />
         </button>
       </div>
-      <section className="photos">
-        <div className="photos__header">
-          <span className="photos__title">Photos</span>
-          <button className="photos__button" type="button">
-            See more
-          </button>
-        </div>
-        <ul className="photos__list">
-          {latestSixPhotos
-            .filter((image, i) => i < 6)
-            .map((image) => (
-              <li
-                key={image.id}
-                className="photos__item"
-                style={{ backgroundImage: `url(${image.urls.thumb})` }}
-                onClick={() =>
-                  changeBoardBackground({
-                    backgroundImage: `url(${image.urls.thumb})`,
-                  })
-                }
+      {!isColorsOptionsOpen ? (
+        <section className="photos">
+          {!isPhotosOptionsOpen ? (
+            <div className="photos__header">
+              <span className="photos__title">Photos</span>
+              <button
+                className="photos__button"
+                type="button"
+                onClick={() => togglePhotosOptions(true)}
               >
-                {boardBackgroundKey === 'backgroundImage' &&
-                boardBackgroundValue === `url(${image.urls.thumb})` ? (
-                  <i className="fas fa-check" />
-                ) : null}
-                <a
-                  className="photos__link"
-                  href={`${
-                    image.user.links.html
-                  }?utm_source=orgello&utm_medium=referral&utm_campaign=api-credit`}
-                  title={`${image.user.name}`}
-                  target="_blank"
-                  rel="noopener noreferrer"
+                See more
+              </button>
+            </div>
+          ) : null}
+          <ul
+            className="photos__list"
+            style={isPhotosOptionsOpen ? { marginTop: '1rem' } : {}}
+          >
+            {latestSixPhotos
+              .filter((image, i) => i < 6)
+              .map((image) => (
+                <li
+                  key={image.id}
+                  className="photos__item"
+                  style={{ backgroundImage: `url(${image.urls.thumb})` }}
+                  onClick={() =>
+                    changeBoardBackground({
+                      backgroundImage: `url(${image.urls.thumb})`,
+                    })
+                  }
                 >
-                  {image.user.name}
-                </a>
-              </li>
-            ))}
-        </ul>
-      </section>
-      <section className="colors">
-        <div className="colors__header">
-          <span className="colors__title">Colors</span>
-          <button className="colors__button" type="button">
-            See more
-          </button>
-        </div>
-        <ul className="colors__list">
-          {colors
-            .filter((c, i) => i < 6)
-            .map((color) => (
+                  {boardBackgroundKey === 'backgroundImage' &&
+                  boardBackgroundValue === `url(${image.urls.thumb})` ? (
+                    <i className="fas fa-check" />
+                  ) : null}
+                  <a
+                    className="photos__link"
+                    href={`${
+                      image.user.links.html
+                    }?utm_source=orgello&utm_medium=referral&utm_campaign=api-credit`}
+                    title={`${image.user.name}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    {image.user.name}
+                  </a>
+                </li>
+              ))}
+          </ul>
+        </section>
+      ) : null}
+
+      {!isPhotosOptionsOpen ? (
+        <section className="colors">
+          {!isColorsOptionsOpen ? (
+            <div className="colors__header">
+              <span className="colors__title">Colors</span>
+              <button
+                className="colors__button"
+                type="button"
+                onClick={() => toggleColorsOptions(true)}
+              >
+                See more
+              </button>
+            </div>
+          ) : null}
+          <ul
+            className="colors__list"
+            style={isColorsOptionsOpen ? { marginTop: '1rem' } : {}}
+          >
+            {modifiedColors.map((color) => (
               <li
                 key={color.id}
                 className="colors__item"
@@ -93,8 +156,9 @@ const BackgroundOptions = ({
                 ) : null}
               </li>
             ))}
-        </ul>
-      </section>
+          </ul>
+        </section>
+      ) : null}
     </div>
   );
 };
