b124333c0f963ab4a264c2c2d5606df1a57399a5
= <7rubengarcia7@gmail.com>
2019-05-14 06:40:20 -0700

    removed everything except BoardList from Dashboard

--- client/src/components/Dashboard/Dashboard.js
+++ client/src/components/Dashboard/Dashboard.js
@@ -21,27 +21,11 @@ const Dashboard = ({ history, user, board, clearBoard }) => {
   }
 
   return (
-    <section className="dashboard">
-      <section className="dashboard__inner">
-        <ul className="dashboard__nav">
-          <li className="dashboard__link">
-            <i className="fab fa-trello" />
-            <span className="icon-text">Boards</span>
-          </li>
-          <li className="dashboard__link">
-            <i className="far fa-user" />
-            <span className="icon-text">Profile</span>
-          </li>
-          <li className="dashboard__link">
-            <i className="fas fa-cog" />
-            <span className="icon-text">Settings</span>
-          </li>
-        </ul>
-      </section>
+    <div className="dashboard">
       <section className="container">
         <BoardsList />
       </section>
-    </section>
+    </div>
   );
 };
 
