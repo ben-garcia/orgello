8b85351c31621c91c5c3391ee0a092b967f0b027
Ben <7rubengarcia7@gmail.com>
2026-04-12 15:12:19 -0700

    chore: move server to its own repository

--- client/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: ['airbnb', 'plugin:prettier/recommended'],
-  env: {
-    browser: true,
-  },
-  rules: {
-    'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-  },
-};
